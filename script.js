93ba6abeeb23361efe5fccccbc0e7fd5b12dd162
Din <DINKH@ad.vzwcorp.com>
2017-08-15 13:24:48 -0400

    Add time and day of week

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 var today = new Date();
 var date = today.getDate();
 var year = today.getFullYear();
+var time = today.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true });
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
+var dayNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
 var month = monthNames[today.getMonth()];
+var day = dayNames[today.getDay()];
 
 today = month + ' ' + date + ', ' + year;
+document.getElementById('day').innerHTML = day;
 document.getElementById('date').innerHTML = today;
+document.getElementById('time').innerHTML = time;
